2b079f95fbb11dcfa717acdfd208ebc0631c64eb
Yannick Scherer <yannick.scherer@gmail.com>
2013-05-16 07:07:49 +0200

    Added tests for bytes->hex.

--- test/pandect/core_test.clj
+++ test/pandect/core_test.clj
@@ -5,6 +5,15 @@
         pandect.core))
 
 (tabular
+  (fact "about bytes->hex"
+    (let [^"[B" d (byte-array (map byte ?data))]
+      (bytes->hex d) => ?result))
+  ?data                ?result
+  [0 0 0 0]            "00000000"  
+  [-1 7 16 64]         "ff071040"
+  [-5 127 4 15]        "fb7f040f")
+
+(tabular
   (fact "about hashing 'Hello World!'"
     (?digest "Hello World!") => ?result)
   ?digest       ?result
