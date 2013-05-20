d2f25d66425a25fe50fd00a877786dec9c5664a9
Yannick Scherer <yannick.scherer@gmail.com>
2013-05-20 10:20:26 +0200

    Release 0.2.1

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,9 @@
 ## Changelog
 
+### 0.2.1
+
+- Adler32 and CRC32 now create a byte array directly, not via a `ByteBuffer`
+
 ### 0.2.0
 
 - added abstraction (i.e. protocol) `Digest`
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pandect "0.2.1-SNAPSHOT"
+(defproject pandect "0.2.1"
   :description "Message Digest and Checksum Library for Clojure"
   :url "https://github.com/xsc/pandect"
   :license {:name "Eclipse Public License"
