cb96fe81563c075470e00dcfbae3c8b38b2c2ae9
Yannick Scherer <yannick.scherer@stylefruits.de>
2014-03-22 17:08:06 +0100

    fix whitespace.

--- test/pandect/core_test.clj
+++ test/pandect/core_test.clj
@@ -9,6 +9,6 @@
     (let [^"[B" d (byte-array (map byte ?data))]
       (bytes->hex d) => ?result))
   ?data                ?result
-  [0 0 0 0]            "00000000"  
+  [0 0 0 0]            "00000000"
   [-1 7 16 64]         "ff071040"
   [-5 127 4 15]        "fb7f040f")
