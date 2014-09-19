90b0e23affa0eeecc5a1b2b39704634dfbd9e152
Jan Stępień <jan.stepien@stylefruits.de>
2014-09-19 12:04:28 +0200

    Split sources per algorithm and generate them AOT
    This patch allows loading code on an per-algorithm granularity level.
    (require 'pandect.algo.sha1) for instance loads only SHA-1 functions.
    
    This allows to load algorithms on a per need basis without compilation of
    entire pandect.core.
    
    The patch maintains backwards compatibility of the API.

--- src/clojure/pandect/core.clj
+++ src/clojure/pandect/core.clj
@@ -1,54 +1,29 @@
 (ns ^{:doc "Digest Creation for Pandect"
       :author "Yannick Scherer"}
   pandect.core
-  (:require [pandect.gen
-             [core :refer [generate get-code-generators]]
-             [hash-generator :refer [hash-generator]]
-             [hmac-generator :refer [hmac-generator]]]
-            [pandect.impl message-digest checksum bouncy-castle]))
+  (:require [pandect.utils.buffer :refer [*buffer-size*]]
+            [potemkin :refer [import-vars]]
+            [pandect.codegen :refer [algorithms]]))
 
 (set! *warn-on-reflection* true)
 (set! *unchecked-math* true)
 
-;; ## Buffer Size
+;; ## Re-export all algorithms
 
-(def ^:dynamic *buffer-size*
-  "Buffer Size for Stream Processing"
-  2048)
-
-(defmacro with-buffer-size
-  "Set buffer size for stream processing."
-  [n & body]
-  `(binding [*buffer-size* ~n]
-     ~@body))
-
-;; ## Available Algorithms
-
-(def ^:private algorithms
-  '{md5       "MD5"           md2       "MD2"
-    md4       "MD4"           gost      "GOST 34.11-94"
-    sha1      "SHA-1"         sha224    "SHA-224"
-    sha256    "SHA-256"       sha384    "SHA-384"
-    sha512    "SHA-512"       sha3-256  "SHA-3 (256)"
-    sha3-224  "SHA-3 (224)"   sha3-384  "SHA-3 (384)"
-    sha3-512  "SHA-3 (512)"   whirlpool "Whirlpool"
-    adler32   "ADLER-32"      crc32     "CRC-32"
-    ripemd128 "RIPEMD-128"    ripemd160 "RIPEMD-160"
-    ripemd256 "RIPEMD-256"    ripemd320 "RIPEMD-320"
-    tiger     "Tiger (192,3)" siphash   "SipHash-2-4"
-    siphash48 "SipHash-4-8"})
-
-(def ^:private generators
-  [hash-generator
-   hmac-generator])
-
-(defmacro ^:private generate-hash-functions!
+(defmacro ^:private reexport-algos
   []
-  `(do
-     ~@(for [[sym algorithm] algorithms
-             generator generators
-             code-gen (get-code-generators algorithm)]
-         (generate generator code-gen sym `*buffer-size*))
-     nil))
+  (list*
+    `import-vars
+    (for [algo (algorithms)
+          :let [ns (symbol (str 'pandect.algo. algo))]]
+      (do
+        (require ns)
+        (list*
+          ns
+          (for [[sym _] (ns-publics (symbol (str 'pandect.algo. algo)))
+                :when (.startsWith (str sym) (str algo))]
+            sym))))))
 
-(generate-hash-functions!)
+(reexport-algos)
+
+(import-vars pandect.utils.buffer/with-buffer-size)
