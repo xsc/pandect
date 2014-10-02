18b61ad3714377000cc655b4b49470680a555ad4
Yannick Scherer <yannick.scherer@stylefruits.de>
2014-10-02 09:44:16 +0200

    add ':pedantic?' flag; test against Clojure 1.5.1.

--- project.clj
+++ project.clj
@@ -6,9 +6,11 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [potemkin "0.3.9"]
                  [org.bouncycastle/bcprov-jdk15on "1.51"]]
+  :exclusions [org.clojure/clojure]
   :source-paths ["src/clojure" "target/generated"]
   :java-source-paths ["src/java"]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]
+  :profiles {:dev {:dependencies [[midje "1.6.3"]
+                                  [joda-time "2.4"]]
                    :plugins [[lein-midje "3.1.3"]
                              [codox "0.8.9"]]
                    :codox {:include [pandect.core]}}
@@ -16,8 +18,11 @@
                                         [clj-message-digest "1.0.0"]
                                         [digest "1.4.4"]]
                          :source-paths ["shootout"]
-                         :jvm-opts ["-Xmx1g" "-server"]}}
+                         :jvm-opts ["-Xmx1g" "-server"]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
   :prep-tasks ["codegen"]
   :aliases {"benchmark" ["with-profile" "dev,benchmark" "run" "-m"]
             "codegen" ["run" "-m" "pandect.codegen"]
-            "test" ["with-profile" "+dev" "midje"]})
+            "all" ["with-profile" "+dev:+1.4:+1.5"]
+            "test" ["with-profile" "+dev" "midje"]}
+  :pedantic? :abort)
