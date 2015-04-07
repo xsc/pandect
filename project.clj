460f6f21d321fa4ea37a832364b05405f5e626a0
Yannick Scherer <yannick.scherer@stylefruits.de>
2015-04-07 19:47:16 +0200

    Version 0.5.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pandect "0.5.2-SNAPSHOT"
+(defproject pandect "0.5.2"
   :description "Message Digest and Checksum Library for Clojure"
   :url "https://github.com/xsc/pandect"
   :license {:name "MIT License"
