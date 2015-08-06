282b1af55fa62d0b354093edd9bd4582ced412fb
Yannick Scherer <yannick.scherer@stylefruits.de>
2015-08-06 16:48:21 +0200

    upgrade dependencies.

--- project.clj
+++ project.clj
@@ -5,14 +5,14 @@
             :url "http://opensource.org/licenses/MIT"
             :year 2014
             :key "mit"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [potemkin "0.3.13"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [potemkin "0.4.1"]
                  [org.bouncycastle/bcprov-jdk15on "1.52"]]
   :exclusions [org.clojure/clojure]
   :source-paths ["src/clojure" "target/generated"]
   :java-source-paths ["src/java"]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]
-                                  [joda-time "2.7"]]
+  :profiles {:dev {:dependencies [[midje "1.7.0"]
+                                  [joda-time "2.8.1"]]
                    :plugins [[lein-midje "3.1.3"]
                              [codox "0.8.10"]]
                    :codox {:project {:name "pandect"}} }
@@ -21,11 +21,12 @@
                                         [digest "1.4.4"]]
                          :source-paths ["shootout"]
                          :jvm-opts ^:replace ["-Xmx1g" "-server"]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}}
 
   :prep-tasks ["codegen"]
   :aliases {"benchmark" ["with-profile" "dev,benchmark" "run" "-m"]
             "codegen" ["run" "-m" "pandect.codegen"]
-            "all" ["with-profile" "+dev:+1.5"]
-            "test" ["with-profile" "+dev" "midje"]}
+            "all" ["with-profile" "+dev:+1.5:+1.6"]
+            "test" ["midje"]}
   :pedantic? :abort)
