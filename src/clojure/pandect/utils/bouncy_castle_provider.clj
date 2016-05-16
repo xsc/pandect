cc76ad41a932f9fad71063cb0a8a50bd5d587a3d
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-05-16 16:44:42 +0200

    add utility namespace that loads the BouncyCastle provider.

---
+++ src/clojure/pandect/utils/bouncy_castle_provider.clj
@@ -0,0 +1,6 @@
+(ns pandect.utils.bouncy-castle-provider
+  (:import java.security.Security
+           org.bouncycastle.jce.provider.BouncyCastleProvider))
+
+(defonce __install__
+  (Security/addProvider (BouncyCastleProvider.)))
