42f0302a2b29d1a3a364bea91429c4c8f26040f9
Yannick Scherer <yannick.scherer@futurice.com>
2021-01-17 15:04:33 +0100

    Adapt codebase, use Github Actions
    - Adjust directory structure.
    - Use 'lein-isolate'/'mranderson' to include potemkin (instead of
      manually).
    - Generate sources in 'src/pandect/algo' (necessary for mranderson to
      pick them up).
    - Use kaocha for testing.

--- src/pandect/core.clj
+++ src/pandect/core.clj
@@ -2,7 +2,7 @@
       :author "Yannick Scherer"}
   pandect.core
   (:require [pandect.buffer :refer [*buffer-size*]]
-            [pandect.utils.potemkin :refer [import-vars]]
+            [potemkin :refer [import-vars]]
             [pandect.codegen :refer [algorithms algorithm-namespace]]))
 
 (set! *warn-on-reflection* true)
